ef0aa79e365e2fc31d53f657154d82bc0aa53f91
jsuci <jsuci.jsuci@gmail.com>
2023-02-07 18:30:18 +0800

    fix getting results

--- index.js
+++ index.js
@@ -14,21 +14,27 @@ const url2 = 'https://www.pinoyswertres.com/swertres-result-history-';
 
   // Load existing data from the CSV file
   let existingData = [];
-  fs.createReadStream('data.csv')
-    .pipe(csvParser({skipLines: 1, headers: ['date', '2PM', '4PM', '9PM']}))
-    .on('data', data => {
-      existingData.push(data);
-    })
-    .on('end', () => {
-      console.log(existingData);
-    });
+  fs.access('data.csv', fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.writeFileSync('data.csv', 'date,twoPM,fourPM,ninePM\n', 'utf8');
+    }
+    fs.createReadStream('data.csv')
+      .pipe(csvParser({ skipLines: 1, headers: ['date', 'twoPM', 'fourPM', 'ninePM'] }))
+      .on('data', data => {
+        existingData.push(data);
+      })
+      .on('end', () => {
+        console.log(`Reading ${existingData.length} entries from file`);
+      });
+  });
+  
 
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   const currentYear = new Date().getFullYear();
-  let startYear = 2020;
+  let startYear = 2009;
 
   if (!arg1) {
     startYear = currentYear;
@@ -56,27 +62,32 @@ const url2 = 'https://www.pinoyswertres.com/swertres-result-history-';
           const date = new Date(dateString);
           // const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' });
           const formattedDate = date.toLocaleDateString('default', { month: 'short', day: '2-digit', year: 'numeric' });
-          
+
           return {
             date: `'${formattedDate}'`,
-            twoPM: `'${tdArray[1]}'`,
-            fourPM: `'${tdArray[2]}'`,
-            ninePM: `'${tdArray[3]}'`,
+            twoPM: `'${tdArray[1].replace(/-/g, "")}'`,
+            fourPM: `'${tdArray[2].replace(/-/g, "")}'`,
+            ninePM: `'${tdArray[3].replace(/-/g, "")}'`,
           };
+
         });
+    });
+
+    const filteredResults = results.filter(result => {
+      return !result.date.includes("Invalid Date");
     });
 
     startYear++;
 
-    console.log(`Add results to existing data...\n`, results);
+    console.log(`Added ${filteredResults.length} results to ${existingData.length} existing data...`);
 
     // Add new data to the existing data
-    results.forEach(data => {
-      if (data.date === 'DATE') return;
+    filteredResults.forEach(data => {
 
+      // console.log(data)
 
       // Check if the data already exists in the existing data
-      const existingDataIndex = existingData.findIndex(existingData => existingData.date === data.date);
+      const existingDataIndex = existingData.findIndex(existingData => existingData.date === data.date && existingData.twoPM === data.twoPM);
 
       // If the data exists, replace it with the new data
       if (existingDataIndex !== -1) {
@@ -84,25 +95,31 @@ const url2 = 'https://www.pinoyswertres.com/swertres-result-history-';
       }
       // If the data doesn't exist, add it to the existing data
       else {
+        // console.log(data, 'is added...')
         existingData.push(data);
       }
+    });
+
+    // Sort data base on date
+    existingData.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date);
     });
 
 
     const csvWriter = createCsvWriter({
       path: 'data.csv',
       header: [
-        {id: 'date', title: 'DATE'},
-        {id: 'twoPM', title: '2PM'},
-        {id: 'fourPM', title: '4PM'},
-        {id: 'ninePM', title: '9PM'},
+        {id: 'date', title: 'date'},
+        {id: 'twoPM', title: 'twoPM'},
+        {id: 'fourPM', title: 'fourPM'},
+        {id: 'ninePM', title: 'ninePM'},
       ],
       append: false
     });
 
     csvWriter.writeRecords(existingData)
       .then(() => {
-        console.log('Data added to the CSV file...');
+        console.log(`${existingData.length} data added to the CSV file...\n`);
     });
 
   }
